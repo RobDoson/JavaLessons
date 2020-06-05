b55812b46a88a0e60e95580659befdb891c0cca1
RobDoson <54075431+RobDoson@users.noreply.github.com>
2020-06-05 00:17:42 -0500

    Add files via upload
    Initial commit

---
+++ JavaLessons/src/main/java/com/robdoson/mx/javalessons/Main.java
@@ -0,0 +1,12 @@
+package com.robdoson.mx.javalessons;
+
+/**
+ *
+ * @author RobRS
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
